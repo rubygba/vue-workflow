58f34b66dac4a71dff441d1a31a8e16498b7668d
rubygba <rubygba@foxmail.com>
2016-04-29 09:35:38 +0800

    fix eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,33 @@
 module.exports = {
   root: true,
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-    extends: 'standard',
+
+  //extends: 'standard',
+"parserOptions": {
+  "ecmaVersion": 6,
+  "ecmaFeatures": {
+    "experimentalObjectRestSpread": true,
+    "jsx": true
+  },
+  "sourceType": "module"
+},
+
+"env": {
+  "es6": true,
+  "node": true
+},
+
+"plugins": [
+  "standard",
+  "promise"
+],
+
+"globals": {
+  "document": false,
+  "navigator": false,
+  "window": false
+},
+
   // required to lint *.vue files
   plugins: [
     'html'
@@ -11,7 +37,14 @@ module.exports = {
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // 警告非4个空格tab
-    "indent": [1, 4, { "SwitchCase": 1 }],
+    "indent": [0, 4, { "SwitchCase": 1 }],
+    //
+    "key-spacing": [0, { "beforeColon": false, "afterColon": true }],
+    "no-mixed-spaces-and-tabs": 1,
+    "no-unused-vars": [1, { "vars": "all", "args": "none" }],
+    "quotes": [0, "single", "avoid-escape"],
+    "semi": [0, "never"],
+    "space-before-blocks": [1, "always"],
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
--- README.md
+++ README.md
@@ -16,7 +16,8 @@ npm run dev
 
 # build for production with minification
 npm run build
-
 ```
+
+config.js配置测试服务器地址、转发规则
 
 For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
